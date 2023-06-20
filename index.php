eefe2d1eff7c5d9f48649b4bff2958738a73e163
AlessioAbbati <alex_117@live.it>
2023-06-20 16:06:41 +0200

    add food kennel and games

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 include __DIR__ . '/models/products.php';
-include __DIR__ . '/data.php'
+include __DIR__ . '/models/foods.php';
+include __DIR__ . '/models/games.php';
+include __DIR__ . '/models/kennels.php';
+include __DIR__ . '/data.php';
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,15 +18,15 @@ include __DIR__ . '/data.php'
     <!-- link di BOOTSTRAP, per accedere alla libreria-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <!-- script con link di BOOTSTRAP, da mettere in fondo al body o nell'head con defer -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N"
-    crossorigin="anonymous" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous" defer></script>
 </head>
+
 <body>
     <ul>
         <li>
-            
+
         </li>
     </ul>
 </body>
+
 </html>
