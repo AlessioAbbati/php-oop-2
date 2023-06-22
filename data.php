9e05ac20ffeab8feeadbbf09e9f6163d176c282f
AlessioAbbati <alex_117@live.it>
2023-06-22 10:12:28 +0200

    mod right weight

--- data.php
+++ data.php
@@ -13,7 +13,7 @@ $arrProducts = [
     new Toy(1, 'Palla', 'Un classico evergreen', 500, $categories[1], 'percorso immagine', 'plastica', 'rosso', 5000),
 	new Toy(1, 'Pollo squiccante', 'Dallo al tuo cane e non avrai più un momento di pace', 700, $categories[1], 'percorso immagine', 'plastica', 'rosso', 200),
 
-    new Shelter(1, 'Cuccetta', 'La pricavacy del tuo animale è la nostra priorità', 5550, $categories[0], 'percorso immagine', 500, 700, 600, -500),
+    new Shelter(1, 'Cuccetta', 'La pricavacy del tuo animale è la nostra priorità', 5550, $categories[0], 'percorso immagine', 500, 700, 600, 500),
 	new Shelter(1, 'Suite per animali', 'Più spazio per animali esigenti', 1000, $categories[1], 'percorso immagine', 500, 700, 600, 800),
 	
 ];
